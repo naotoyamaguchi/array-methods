ea321e0182dc5d7eb3ad397b2a06db7ddb2b43c0
Naoto Yamaguchi <naotoy@hawaii.edu>
2016-11-06 22:18:29 -1000

    finished

--- arrayMethods.js
+++ arrayMethods.js
@@ -52,3 +52,14 @@ var updatedOrders = orderQueue.concat(extraOrders);
 
 var sentence = scrambledWords.join(' ');
 var myFruits = fruitCollection.join(' + ');
+
+var favoriteFriends = friends.slice(1, 3);
+var owesMoney = friends.slice(3, 6);
+
+var monthNameString = monthNames.toString();
+
+var favoriteColor = colors.indexOf('Red');
+var favoriteEvenNumber = mixedNums.indexOf(76);
+
+var lastNine = bulkNumbers.lastIndexOf(9);
+var lastFive = bulkNumbers.lastIndexOf(5);
